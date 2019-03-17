a0516dd55a14da90f0c3f2198ce4a8c18b76f28a
Will Huang <will_huang@trend.com.tw>
2019-03-17 12:23:51 +0800

    Produce JSON message

--- src/main/java/com/will/rabbitmqproducer/RabbitmqProducerApplication.java
+++ src/main/java/com/will/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -1,5 +1,7 @@
 package com.will.rabbitmqproducer;
 
+import com.will.rabbitmqproducer.entity.Employee;
+import com.will.rabbitmqproducer.producer.EmployeeJsonProducer;
 import com.will.rabbitmqproducer.producer.HelloRabbitProducer;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,19 +10,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Date;
+
 @SpringBootApplication
-@EnableScheduling
 public class RabbitmqProducerApplication implements CommandLineRunner {
 
-//	@Autowired
-//	private HelloRabbitProducer helloRabbitProducer;
-
-
-
+	@Autowired
+	private EmployeeJsonProducer employeeJsonProducer;
 
 	@Override
 	public void run(String... args) throws Exception {
 //		helloRabbitProducer.sendHello("Will" + Math.random());;
+		for (int i = 0; i < 5 ; i++) {
+			Employee e = new Employee("emp" + i, "Employee " + i , new Date());
+			employeeJsonProducer.sendMessage(e);
+		}
 	}
 
 	public static void main(String[] args) {
