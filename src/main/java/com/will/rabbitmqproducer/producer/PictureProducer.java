450e26f8c2cc87f28894ad3fe8cdfe6e23c6a516
Will Huang <will_huang@trend.com.tw>
2019-03-24 22:45:40 +0800

    Add topic

--- src/main/java/com/will/rabbitmqproducer/producer/PictureProducer.java
+++ src/main/java/com/will/rabbitmqproducer/producer/PictureProducer.java
@@ -20,8 +20,24 @@ public class PictureProducer {
 	private ObjectMapper objectMapper = new ObjectMapper();
 
 	public void sendMessage(Picture p) throws JsonProcessingException{
+		StringBuilder sb = new StringBuilder()	;
+		sb.append(p.getSource()); // mobile or web
+		sb.append('.');
+
+		if(p.getSize() > 4000) { //large or small
+			sb.append("large");
+		}else {
+			sb.append("small");
+		}
+
+		sb.append(".");
+		sb.append(p.getType()); //jpg, png or svg
+
+
+		String routingKey = sb.toString();
+
 		String json = objectMapper.writeValueAsString(p);
-		rabbitTemplate.convertAndSend("x.picture", p.getType(), json);
+		rabbitTemplate.convertAndSend("x.picture2", routingKey, json);
 	}
 
 }
